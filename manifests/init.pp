83e7f69065c6953cac51eb580e6fc3535ac35b39
Eivind Uggedal <eivind@uggedal.com>
2011-07-21 16:20:17 +0200

    Actual vim configuration.

---
+++ manifests/init.pp
@@ -0,0 +1,32 @@
+class vim {
+  package { "vim-nox":
+    ensure => present,
+  }
+
+  # Syntax checker for python used with the syntastic vim plugin:
+  package { "pyflakes":
+    ensure => present,
+  }
+
+  # Ignore git files for bundles as git submodules:
+  File { ignore => '.git' }
+
+  file { "/etc/vim/vimrc":
+    content => template("vim/vimrc.erb"),
+    require => Package["vim-nox"],
+  }
+
+  file {'/usr/share/vim/vimcurrent/autoload':
+    source => "puppet:///modules/vim/autoload",
+    recurse => true,
+    mode => 644,
+    require => File["/etc/vim/vimrc"],
+  }
+
+  file {'/usr/share/vim/vimcurrent/bundle':
+    source => "puppet:///modules/vim/bundle",
+    recurse => true,
+    mode => 644,
+    require => File["/etc/vim/vimrc"],
+  }
+}
